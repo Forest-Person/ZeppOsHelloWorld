407fb07dfa24aca5ece328ade0d0d26cde4343d0
matthew <permahuman@gmail.com>
2023-09-05 23:19:33 -0700

    Create and label two buttons to one to generate integer and one to generate a decimal on button push.

--- page/index.js
+++ page/index.js
@@ -1,48 +1,74 @@
 
 
 
-import { createWidget, widget, align, prop, text_style, event } from '@zos/ui'
+import { createWidget,  widget, align, prop, text_style, event } from '@zos/ui'
 
 Page({
   build() {
  
+const RandomButton = createWidget(widget.BUTTON, { //when randomButton is pressed it changes randomtext widget with event listener below to random decimal number
+  //Notice addevent listeners are added at the end of the module after the widgets added
 
-    const RandomButton = createWidget(widget.BUTTON, {
-      x: 96,
-      y: 300,
-      w: 288,
-      h: 46,
-      color: 0xffffff,
-      text_size: 36,
-      align_h: align.CENTER_H,
-      align_v: align.CENTER_V,
-      text_style: text_style.NONE,
-      text:'Butt'
-      
-    })
+x: 96,
+y: 50,
+w: 300,
+h: 60,
+color: 0xffffff,
+text_size: 36,
+align_h: align.CENTER_H,
+align_v: align.CENTER_V,
+text_style: text_style.NONE,
+text:"Make decimal",
 
-    
 
-const RandomText = createWidget(widget.TEXT, {
-  x: 96,
-  y: 150,
-  w: 288,
-  h: 200,
+
+})
+
+const RandomButton2 = createWidget(widget.BUTTON, { //when randomButton is pressed it changes randomtext widget with event listener below to random decimal number
+  //Notice addevent listeners are added at the end of the module after the widgets added
+
+x: 96,
+y: 150,
+w: 300,
+h: 60,
+color: 0xffffff,
+text_size: 36,
+align_h: align.CENTER_H,
+align_v: align.CENTER_V,
+text_style: text_style.NONE,
+text:"Make 1-10",
+
+
+
+})
+
+const RandomText = createWidget(widget.TEXT, { //When random text is pressed a new number from 1-4 will be randomly selected
+  //The locations of the buttons are relative to the coordinates below. no stacking is demonstrated here
+  x: 50,
+  y: 300,
+  w: 350,
+  h: 60,
   color: 0xffffff,
   text_size: 36,
   align_h: align.CENTER_H,
   align_v: align.CENTER_V,
   text_style: text_style.NONE,
-  text:'BORCHEs'
+  
   
 })
 
+RandomButton2.addEventListener(event.CLICK_DOWN,()=>
+  RandomText.setProperty(prop.MORE,{text:Math.floor(Math.random()*11)})
+  )
 
 
+RandomButton.addEventListener(event.CLICK_DOWN,()=>
+  RandomText.setProperty(prop.MORE,{text:Math.random()})
+  )
   
-RandomText.addEventListener(event.CLICK_DOWN, (info) => {
-  let num = Math.floor(Math.random()*5)
-RandomText.setProperty(prop.MORE,{text: num})
+RandomText.addEventListener(event.CLICK_DOWN, () => {
+  
+RandomText.setProperty(prop.MORE,{text: Math.floor(Math.random()*5)})
 })
 
 }
